8f024355ba3b271052e4f3b588802b4be6239826
wrooot <wrooot@users.noreply.github.com>
2019-05-30 20:46:36 +0300

    #3 increase size of lastipaddress to support ipv6

--- src/database/user-status_mysql.sql
+++ src/database/user-status_mysql.sql
@@ -3,7 +3,7 @@ CREATE TABLE userStatus (
     resource        VARCHAR(64)         NOT NULL,
     online          TINYINT             NOT NULL,
     presence        CHAR(15),
-    lastIpAddress   CHAR(15)            NOT NULL,
+    lastIpAddress   CHAR(40)            NOT NULL,
     lastLoginDate   CHAR(15)            NOT NULL,
     lastLogoffDate  CHAR(15),
     PRIMARY KEY pk_userStatus (username, resource)
@@ -13,7 +13,7 @@ CREATE TABLE userStatusHistory (
     historyID       BIGINT              NOT NULL,
     username        VARCHAR(64)         NOT NULL,
     resource        VARCHAR(64)         NOT NULL,
-    lastIpAddress   CHAR(15)            NOT NULL,
+    lastIpAddress   CHAR(40)            NOT NULL,
     lastLoginDate   CHAR(15)            NOT NULL,
     lastLogoffDate  CHAR(15)            NOT NULL,
     PRIMARY KEY pk_userStatusHistory (historyID)
--- src/database/user-status_postgresql.sql
+++ src/database/user-status_postgresql.sql
@@ -3,7 +3,7 @@ CREATE TABLE userStatus (
     resource VARCHAR(64) NOT NULL,
     online INTEGER NOT NULL,
     presence CHAR(15),
-    lastIpAddress CHAR(15) NOT NULL,
+    lastIpAddress CHAR(40) NOT NULL,
     lastLoginDate CHAR(15) NOT NULL,
     lastLogoffDate CHAR(15),
     constraint pk_userStatus PRIMARY KEY  (username, resource)
@@ -13,7 +13,7 @@ CREATE TABLE userStatusHistory (
     historyID BIGINT NOT NULL,
     username VARCHAR(64) NOT NULL,
     resource VARCHAR(64) NOT NULL,
-    lastIpAddress CHAR(15) NOT NULL,
+    lastIpAddress CHAR(40) NOT NULL,
     lastLoginDate CHAR(15) NOT NULL,
     lastLogoffDate CHAR(15) NOT NULL,
     constraint pk_userStatusHistory PRIMARY KEY (historyID)
