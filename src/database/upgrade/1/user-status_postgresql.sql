a559b7da70ca1fe5d53c9d4af3c5171cad1f383a
wrooot <wrooot@users.noreply.github.com>
2019-05-31 16:20:06 +0300

    #3 ffix postgresql upgrade script

--- src/database/upgrade/1/user-status_postgresql.sql
+++ src/database/upgrade/1/user-status_postgresql.sql
@@ -1,6 +1,6 @@
 # expand lastIpAddress field to support IPv6 addresses
-alter table userStatus modify column lastIpAddress char(45);
-alter table userStatusHistory modify column lastIpAddress char(45);
+alter table userStatus alter column lastIpAddress type char(45);
+alter table userStatusHistory alter column lastIpAddress type char(45);;
 
 # update version
 UPDATE ofVersion SET version = 1 WHERE name = 'user-status';
